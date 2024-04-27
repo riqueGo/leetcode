29c926752c0a10e9e86d95de2f31d6b5da84c5ca
Henrique Gomes <henrique.gomes_de_oliveira@syngenta.com>
2024-04-27 16:22:16 -0300

    insert

--- cpp/InsertInterval/InsertInterval.cpp
+++ cpp/InsertInterval/InsertInterval.cpp
@@ -11,21 +11,20 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
 
         if (intervals.empty())
-            return vector<vector<int>>{newInterval};        
-        
+            return vector<vector<int>>{newInterval};
+
         if(newInterval.at(0) > intervals.at(intervals.size()-1).at(1))
         {
             intervals.push_back(newInterval);
             return intervals;
         }
 
-        int i = 0;
+        int i = 0;    
         vector<vector<int>> result;
         while (i < intervals.size() && newInterval.at(0) > intervals.at(i).at(1))
-        {
-            result.push_back(intervals.at(i));
             i++;
-        }
+        
+        result.insert(result.end(), intervals.begin(), intervals.begin()+i);
 
         while (i < intervals.size() && newInterval.at(1) >= intervals.at(i).at(0))
         {
