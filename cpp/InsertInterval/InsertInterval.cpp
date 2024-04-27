d1c38424972c33eff75f735b5915bb0de9cd30aa
Henrique Gomes <henrique.gomes_de_oliveira@syngenta.com>
2024-04-27 16:16:56 -0300

    insert

---
+++ cpp/InsertInterval/InsertInterval.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include <utility>
+
+using std::vector;
+using std::min;
+using std::max;
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+
+        if (intervals.empty())
+            return vector<vector<int>>{newInterval};        
+        
+        if(newInterval.at(0) > intervals.at(intervals.size()-1).at(1))
+        {
+            intervals.push_back(newInterval);
+            return intervals;
+        }
+
+        int i = 0;
+        vector<vector<int>> result;
+        while (i < intervals.size() && newInterval.at(0) > intervals.at(i).at(1))
+        {
+            result.push_back(intervals.at(i));
+            i++;
+        }
+
+        while (i < intervals.size() && newInterval.at(1) >= intervals.at(i).at(0))
+        {
+            newInterval = {
+                min(newInterval.at(0), intervals.at(i).at(0)),
+                max(newInterval.at(1), intervals.at(i).at(1))
+            };
+            i++;
+        }
+        result.push_back(newInterval);
+        result.insert(result.end(), intervals.begin()+i, intervals.end());
+        
+        return result;
+    }
+};
