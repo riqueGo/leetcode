bb7d73e00b151ca75f191866cbcb8285755d05f3
Henrique Gomes <henrique.gomes_de_oliveira@syngenta.com>
2024-05-04 22:12:41 -0300

    MinStack

--- cpp/MinStack/MinStack.cpp
+++ cpp/MinStack/MinStack.cpp
@@ -9,6 +9,7 @@ public:
     int min;
     Node* next;
 
+    Node(int val) : data(val), next(nullptr) {}
     Node(int val, int min) : data(val), min(min), next(nullptr) {}
 };
 
@@ -33,14 +34,26 @@ public:
             newNode->next = _top;
             _top = newNode;
         }
-
     }
     
     void pop() {
+        Node* temp = _top;
         _top = _top->next;
+        delete temp;
     }
     
     int top() { return _top->data; }
     
     int getMin() { return _top->min; }
 };
+
+int main ()
+{
+    MinStack stack;
+
+    stack.push(1);
+    stack.push(110);
+    stack.push(-1);
+    stack.push(4);
+
+}
