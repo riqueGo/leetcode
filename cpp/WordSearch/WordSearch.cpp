71fced8c174e18997da394cf04213a354a46ba03
Henrique Gomes <henrique.oliveira.gomes@gmail.com>
2024-06-13 18:51:26 -0300

    exist

--- cpp/WordSearch/WordSearch.cpp
+++ cpp/WordSearch/WordSearch.cpp
@@ -7,34 +7,33 @@ using std::to_string;
 
 class Solution {
 private:
-    int directions[4][2] = {{-1,0}, {0,1}, {1,0}, {0,-1}};
-    int columnSize;
-    int rowSize;
     string word;
+    int directions[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+    int rowSize;
+    int columnSize;
 
-    bool isOutOfBoardBounds(int i, int j)
-    {
+    bool isOutOfBound(int i, int j) {
         return i < 0 || j < 0 || i >= columnSize || j >= rowSize;
     }
 
-    bool dfs(vector<vector<char>>& board, const int (&pos)[2], int indexWord)
-    {
-        if(indexWord == word.length())
+    bool dfs(vector<vector<char>>& board, int i, int j, int indexLetter) {
+        if(indexLetter >= word.length()) {
             return true;
-
-        int i = pos[0], j = pos[1];
-        if(isOutOfBoardBounds(i, j) || word[indexWord] != board[i][j])
-            return false;
-        
-        board[i][j] = '#';
-
-        for(int k = 0; k < 4; k++)
-        {
-            if(dfs(board, {directions[k][0] + i, directions[k][1] + j}, indexWord+1))
-                return true;
-            
         }
-        board[i][j] = word[indexWord];
+
+        if(isOutOfBound(i,j) || board[i][j] != word[indexLetter]) {
+            return false;
+        }
+
+        char currentLetter = board[i][j];
+        board[i][j] = '0';
+
+        for(int k = 0; k < 4; k++) {
+            if(dfs(board, directions[k][0]+i, directions[k][1]+j, indexLetter+1)) {
+                return true;
+            }
+        }
+        board[i][j] = currentLetter;
         return false;
     }
 
@@ -43,18 +42,18 @@ public:
         this->word = word;
         columnSize = board.size();
         rowSize = board[0].size();
-        bool foundWord = false;
-        
-        for(int i = 0; i < columnSize; i++)
-        {
-            for(int j = 0; j < rowSize; j++)
-            {
-                if(board[i][j] == word[0] && dfs(board, {i,j}, 0))
+
+        for(int i = 0; i < columnSize; i++) {
+            for(int j = 0; j < rowSize; j++) {
+                if(board[i][j] == word[0] && dfs(board, i, j, 0)) {
                     return true;
+                }
             }
         }
+
         return false;
     }
+
 };
 
 
